6d32c674078697b0a2d4aa804fee4b036e4e478f
Elizandtech <46058821+Elizandtech@users.noreply.github.com>
2019-01-16 19:55:46 -0800

    Update largest_prime-factor.py
    Add code

--- largest_prime-factor.py
+++ largest_prime-factor.py
@@ -1,6 +1,7 @@
-#The prime factors of 13195 are 5, 7, 13 and 29.
-#What is the largest prime factor of the number 600851475143 ?
-    ## Create list of all factors, create list of primes, then find the largest factor from the list.
+#The prime factors of 13195 are 5, 7, 13 and 29. (Use as check for code)
+    ## Create list of prime factors and then find the largest factor from the list.
+
+import math
 
 def is_prime(inp):
     isprime = True
@@ -11,7 +12,23 @@ def is_prime(inp):
     return isprime
 
 
-def print_prime_numbers(inp):
-    for testnumber in range(2, inp+1):
-        if is_prime(testnumber):
-            print(testnumber)
+def return_prime_numbers_in_a_list(lst):
+    return [testnumber for testnumber in lst if is_prime(testnumber)]
+
+
+def return_largest_element_in_list(lst):
+    largest_element = 0
+    for elem in lst:
+        if elem > largest_element:
+            largest_element = elem
+    return largest_element
+
+
+def main():
+    number_to_factor = int(input("Enter a number: "))
+    factors = [factor for factor in range(2, number_to_factor+1) if number_to_factor % factor ==0]
+    print(return_largest_element_in_list(return_prime_numbers_in_a_list(factors)))
+
+if __name__=="__main__":
+    main()
+
